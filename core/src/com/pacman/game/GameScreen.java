6beee9eae4b37412d632968ca5ee157e9d88c17e
Windows 8.1 <Windows 8.1@Windows>
2016-10-07 11:51:53 +0700

    ControlPacman

--- core/src/com/pacman/game/GameScreen.java
+++ core/src/com/pacman/game/GameScreen.java
@@ -1,5 +1,6 @@
 package com.pacman.game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -16,8 +17,20 @@ public class GameScreen extends ScreenAdapter {
 	        y = 100;
 	    }
 	    private void update(float delta) {
-	        x += 5;    
+	        if(Gdx.input.isKeyPressed(Keys.LEFT)) {
+	            x -= 10;
+	        }
+	        if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
+	            x += 10;
+	        }  
+	        if(Gdx.input.isKeyPressed(Keys.UP)) {
+	            y += 10;
+	        } 
+	        if(Gdx.input.isKeyPressed(Keys.DOWN)) {
+	            y -= 10;
+	        } 
 	    }
+	    
 	    @Override
 	    public void render(float delta) {
 	        //System.out.println("Hello " + delta);
