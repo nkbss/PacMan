c658078f695d7253b6f68fd34f05b383f5ec1c2a
Windows 8.1 <Windows 8.1@Windows>
2016-10-09 13:22:46 +0700

    SetPositionAndControl

--- core/src/com/pacman/game/GameScreen.java
+++ core/src/com/pacman/game/GameScreen.java
@@ -5,29 +5,33 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 public class GameScreen extends ScreenAdapter {
 	 private PacmanGame pacmanGame;
 	 private Texture pacmanImg;
-	 private int x;
-	 private int y;
+	 
+	 private Pacman pacman;
+	 
 	    public GameScreen(PacmanGame pacmanGame) {
 	        this.pacmanGame = pacmanGame;
+	 
 	        pacmanImg = new Texture("pacman.png");
-	        x = 100;
-	        y = 100;
+	 
+	        pacman = new Pacman(100,100);
 	    }
+	 
 	    private void update(float delta) {
 	        if(Gdx.input.isKeyPressed(Keys.LEFT)) {
-	            x -= 10;
+	            pacman.move(Pacman.DIRECTION_LEFT);
 	        }
 	        if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
-	            x += 10;
+	        	  pacman.move(Pacman.DIRECTION_RIGHT);
 	        }  
 	        if(Gdx.input.isKeyPressed(Keys.UP)) {
-	            y += 10;
+	        	  pacman.move(Pacman.DIRECTION_UP);
 	        } 
 	        if(Gdx.input.isKeyPressed(Keys.DOWN)) {
-	            y -= 10;
+	        	  pacman.move(Pacman.DIRECTION_DOWN);
 	        } 
 	    }
 	    
@@ -38,8 +42,9 @@ public class GameScreen extends ScreenAdapter {
 	         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 	    	SpriteBatch batch = pacmanGame.batch;
 	    	update(delta);
-	    	batch.begin();
-	        batch.draw(pacmanImg, x, y);
+	        batch.begin();
+	        Vector2 pos = pacman.getPosition();
+	        batch.draw(pacmanImg, pos.x, pos.y);
 	        batch.end();
 	    }  
 }
