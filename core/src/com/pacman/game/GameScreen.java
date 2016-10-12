64c2dec3677833732f59283858211db29f5a5d5e
Windows 8.1 <Windows 8.1@Windows>
2016-10-12 13:07:42 +0700

    AddMazeToWorld

--- core/src/com/pacman/game/GameScreen.java
+++ core/src/com/pacman/game/GameScreen.java
@@ -7,18 +7,23 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 public class GameScreen extends ScreenAdapter {
-	 private PacmanGame pacmanGame;
+	
+	private PacmanGame pacmanGame;
 	 private Texture pacmanImg;
 	 World world;
+
 	 private Pacman pacman;
-	 
+	private WorldRenderer worldRenderer;
+	
 	    public GameScreen(PacmanGame pacmanGame) {
 	        this.pacmanGame = pacmanGame;
 	        pacmanImg = new Texture("pacman.png");
 	        world = new World(pacmanGame);
+	        worldRenderer = new WorldRenderer(pacmanGame,world);
+	        pacman = world.getPacman();
 	    }
 	 
-	    private void update(float delta) {
+	   public void update(float delta) {
 	        if(Gdx.input.isKeyPressed(Keys.LEFT)) {
 	            pacman.move(Pacman.DIRECTION_LEFT);
 	        }
@@ -33,15 +38,10 @@ public class GameScreen extends ScreenAdapter {
 	        } 
 	    }
 	    
-	    @Override
-	    public void render(float delta) {
-	    	 Gdx.gl.glClearColor(0, 0, 0, 1);
-	         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-	    	SpriteBatch batch = pacmanGame.batch;
-	    	update(delta);
-	        batch.begin();
-	        Vector2 pos = pacman.getPosition();
-	        batch.draw(pacmanImg, pos.x, pos.y);
-	        batch.end();
-	    }  
+	   public void render(float delta) {
+	        Gdx.gl.glClearColor(0, 0, 0, 1);
+	        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	        update(delta);
+	        worldRenderer.render(delta);
+	    }
 }
